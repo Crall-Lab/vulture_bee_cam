f33768aa75fddc3a3e7811b3bb2a8b5fef58c5de
James Crall <james.crall@gmail.com>
2023-12-30 14:29:01 -0600

    adding recording script

---
+++ scripts/record_video.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#source ~/Desktop/bombusbox-setup.sh #this command pulls all the environmental variables that we set in setup.sh into this script
+DATE=$(date +"%Y-%m-%d") #This record the current date with format YYYY-MM-DD
+DATETIME=$(date +"%Y-%m-%d_%H-%M-%S") #Records current date and time with format YYYY-MM-DD_HH-mm-SS
+OUTPUT=/mnt/data/"$HOSTNAME"_"$DATE"/"$HOSTNAME"_"$DATETIME".mjpeg
+#OUTPUT="$VIDEO_DIRECTORY"/"$HOSTNAME"_"$DATE"/"$HOSTNAME"_"$DATETIME"."$CODEC"
+
+sudo mkdir -p /mnt/data/"$HOSTNAME"_"$DATE"
+
+#Set to record for 9 minutes and 50 seconds each video
+libcamera-vid --width 4056 --height 3040 --codec mjpeg --save-pts mjpeg -o "$OUTPUT" --nopreview -t 180000 --framerate 4
+echo video recorded!
